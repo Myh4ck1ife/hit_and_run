65cb40951fe5bd2cab3f76134152d3356ba1427d
e <972641428@qq.com>
2017-03-18 17:31:19 +0800

    图片预览更新

--- demo.js
+++ demo.js
@@ -15,5 +15,19 @@
 	}
   }
 */
-var name = document.getElementById("n");
-alert(name);
+//var name = document.getElementById("n");
+//alert(name);
+/*for (var i = 0; i < lovers.length; i++) {
+	var title_txt = lovers[i].getAttribute("title");
+	if (title_txt)
+	{
+		lovers[i].setAttribute("title","稀饭儿");
+		alert(lovers[i].getAttribute("title"));
+	}
+}*/
+function showPic(whichPic){
+var source = whichPic.getAttribute("href");
+var placeholder = document.getElementById("placeholder");
+placeholder.setAttribute("src",source);
+}
+
