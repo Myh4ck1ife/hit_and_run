0728cc4d98d3c7627eb441494dd9465fd2dd3bf3
e <972641428@qq.com>
2017-03-18 14:45:10 +0800

    js第一次更新

---
+++ demo.js
@@ -0,0 +1,19 @@
+//var lovers = document.getElementsByTagName("p");
+/*for (var i = 0; i < lovers.length; i++) {
+	alert(lovers[i].getAttribute("title"));
+}
+*/
+/*for (var i = 0; i < lovers.length; i++) {
+	var title_txt = lovers[i].getAttribute("title");
+    //if (title_txt != null)
+	//{
+		//alert(title_txt);
+	//}
+	if (title_txt)
+	{
+		alert(title_txt);
+	}
+  }
+*/
+var name = document.getElementById("n");
+alert(name);
